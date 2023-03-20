2752b4de55fd95c97563a0a77fceafe0d16015fd
Aorts <t.sappalunn@gmail.com>
2023-03-20 14:35:54 +0700

    complete

---
+++ services/grade_test.go
@@ -0,0 +1,43 @@
+package services_test
+
+import (
+	"goTest/services"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGradeCheck(t *testing.T) {
+
+	type testCase struct {
+		name     string
+		score    int
+		expected string
+	}
+
+	cases := []testCase{
+		{name: "a", score: 80, expected: "A"},
+		{name: "b", score: 70, expected: "B"},
+		{name: "c", score: 60, expected: "C"},
+		{name: "d", score: 50, expected: "D"},
+		{name: "e", score: 40, expected: "E"},
+		{name: "f", score: 10, expected: "E"},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			grade := services.CheckGrade(c.score)
+			assert.Equal(t, c.expected, grade)
+			// expected := c.expected
+			// if grade != expected {
+			// 	t.Errorf("got %v expected %v", grade, expected)
+			// }
+		})
+	}
+}
+
+func BenchmarkCheckGrade(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		services.CheckGrade(80)
+	}
+}
